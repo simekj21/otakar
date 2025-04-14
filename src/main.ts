d1f14399cc72b3e2693f79f18e5bb57ee664d36a
simekj21 <simek.media@gmail.com>
2025-04-14 14:33:18 +0200

    Inital commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.use(router)
+app.mount('#app')
+
+
+
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// const app = createApp(App)
+
+// app.use(router)
+
+// app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import InputText from 'primevue/inputtext';
+
+import { ref } from 'vue';
+
+const text = ref('')
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <h1>Welcome to Your Vue.js App</h1>
+      <InputText v-model="text" placeholder="Enter text" />
+    </div>
+  </main>
+</template>
